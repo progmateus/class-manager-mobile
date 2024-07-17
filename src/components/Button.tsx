7418159cc5c6298fbfbf1d411ecc5e57b2970b98
Mateus Lopes <devmateustools@gmail.com>
2024-07-17 05:38:14 -0300

    feat: custom button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,20 +7,25 @@ type Props = IButtonProps & {
 }
 
 export function Button({ title, variant = "solid", fontSize = "sm", ...rest }: Props) {
+  const { color, bg, bgColor, backgroundColor, background } = rest
+  const useBgProp = bg || bgColor || backgroundColor || background;
+  const definedColor = color ? color : variant === "outline" ? "brand.600" : "white";
+  const definedBg = useBgProp ? useBgProp : variant === "outline" ? "transparent" : "brand.600";
   return (
     <NativeBaseButton
       w="full"
       h={12}
-      bg={variant === "outline" ? "transparent" : "brand.600"}
+      bg={definedBg}
       borderWidth={variant === "outline" ? 1 : 0}
+      borderColor={variant === "outline" ? definedColor : "transparent"}
       rounded="xl"
       _pressed={{
-        bg: variant === "outline" ? "coolGray.100" : "brand.500",
+        bg: variant === "outline" ? "coolGray.100" : definedBg,
       }}
       {...rest}
     >
       <Text
-        color={variant === "outline" ? "brand.600" : "white"}
+        color={definedColor}
         fontFamily="heading"
         fontSize={fontSize}
       >
