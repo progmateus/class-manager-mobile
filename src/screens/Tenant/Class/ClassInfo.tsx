32f06fd9ac6708aa8dc15c9b780e11d16de31c8d
Mateus Lopes <devmateustools@gmail.com>
2024-08-11 02:01:23 -0300

    sdsad

--- src/screens/Tenant/Class/ClassInfo.tsx
+++ src/screens/Tenant/Class/ClassInfo.tsx
@@ -2,28 +2,23 @@ import { ClassInfoItem } from "@components/Class/InfoItem"
 import { MenuItem } from "@components/MenuItem"
 import { PageHeader } from "@components/PageHeader"
 import { ScrollContainer } from "@components/ScrollContainer"
-import { Viewcontainer } from "@components/ViewContainer"
-import { Center, HStack, Icon, Image, Link, SimpleGrid, Text, View, VStack } from "native-base"
+import { SimpleGrid, Text, View, VStack } from "native-base"
 import { GraduationCap, ArrowRight, IdentificationBadge, LinkSimple, Clock, Calendar, CalendarBlank, ArrowsLeftRight } from "phosphor-react-native"
 
 export function ClassInfo() {
-  const classInfo = {
-    name: "aaa",
-    students: [],
-    avatar: "https://img.freepik.com/fotos-gratis/retrato-de-uma-jovem-bonita-em-pe-na-parede-cinza_231208-10760.jpg?ga=GA1.1.1603704743.1686338071&semt=sph"
-  }
   return (
     <View flex={1}>
       <PageHeader title="Turma" />
       <ScrollContainer>
-        <SimpleGrid mt={4} columns={3}>
+        <SimpleGrid mt={4} columns={3} justifyContent="space-between">
           <ClassInfoItem title="ALUNOS" info="18" />
           <ClassInfoItem title="PROFESSORES" info="7" />
           <ClassInfoItem title="AULAS CONCLUÍDAS" info="749" />
           <ClassInfoItem title="AULAS PENDENTES" info="10" />
           <ClassInfoItem title="AULAS CANCLEDAS" info="4" />
         </SimpleGrid>
-        <VStack mt={8} space={4} pb={20}>
+        <VStack space={4} pb={20}>
+          <Text color="coolGray.400" fontSize="md" mb={-2}> Ações</Text>
           <MenuItem.Root>
             <MenuItem.Icon icon={GraduationCap} />
             <MenuItem.Content title="Gerenciar alunos" description="Gerencie os alunos manualmente" />
