c88fc1de4924987db8c54b79010e0fe55e1f9067
Mateus Lopes <devmateustools@gmail.com>
2024-08-14 03:52:06 -0300

    class hour item

---
+++ src/components/Class/HoursItem/ClasshourItem.tsx
@@ -0,0 +1,80 @@
+import { Button, HStack, PresenceTransition, Text, VStack } from "native-base"
+import { CaretDown, TrashSimple } from "phosphor-react-native"
+import { useState } from "react"
+import { TouchableOpacity } from "react-native"
+import { THEME } from "src/theme"
+
+interface IProps {
+  item: {
+    id?: number;
+    dayOfWeek: string;
+    hours: IHour[]
+  }
+}
+
+interface IHour {
+  start: string;
+  end: string
+}
+export function ClassHourItem({ item: { dayOfWeek, hours } }: IProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const transforWeekDay = (dayOfWeek: string | number) => {
+    const objTransform: any = {
+      '1': 'DOMINGO',
+      '2': 'SEGUNDA-FEIRA',
+      '3': 'TERÇA-FEIRA',
+      '4': 'QUARTA-FEIRA',
+      '5': 'QUINTA-FEIRA',
+      '6': 'SEXTA-FEIRA',
+      '7': 'SÁBADO'
+    }
+
+    return objTransform[String(dayOfWeek)]
+  }
+
+  return (
+    <VStack space={4}>
+      <VStack borderWidth={0.5} borderColor="coolGray.400" borderRadius={7} p={6}>
+        <TouchableOpacity onPress={() => setIsOpen(!isOpen)}>
+          <HStack justifyContent="space-between" alignItems="center">
+            <Text>{transforWeekDay(dayOfWeek)}</Text>
+            <CaretDown color={THEME.colors.coolGray[400]} />
+          </HStack>
+        </TouchableOpacity>
+
+        {
+          isOpen && (
+            <PresenceTransition visible={isOpen} initial={{
+              opacity: 0
+            }} animate={{
+              opacity: 1,
+              transition: {
+                duration: 100
+              }
+            }}>
+              <VStack px={8} space={4} mt={6}>
+                {
+                  hours && hours.length && (
+                    hours.map((hour) => {
+                      return (
+                        <HStack key={hour.start} alignItems="center" justifyContent="space-evenly">
+                          <Text fontSize="lg">{`${hour.start} - ${hour.end}`}</Text>
+                          <TrashSimple size={24} />
+                        </HStack>
+                      )
+                    })
+                  )
+                }
+                <Button alignSelf="center" size="md" w="1/2" variant="outline" color="brand.500" borderColor="brand.500" mt={4} rounded="full" borderStyle="dashed">
+                  Adicionar +
+                </Button>
+
+              </VStack>
+            </PresenceTransition>
+          )
+        }
+      </VStack>
+    </VStack>
+  )
+}
--- src/screens/TenantRoutes/classHours/ClassHoursList.tsx
+++ src/screens/TenantRoutes/classHours/ClassHoursList.tsx
@@ -1,19 +1,67 @@
-import { ClassDayHourItem } from "@components/Class/HoursItem/ClassDayHourItem";
+import { ClassHourItem } from "@components/Class/HoursItem/ClasshourItem";
 import { PageHeader } from "@components/PageHeader";
 import { ScrollContainer } from "@components/ScrollContainer";
-import { HStack, PresenceTransition, Text, View, VStack } from "native-base";
-import { ArrowDown, CaretDown, Trash, TrashSimple } from "phosphor-react-native";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native";
-import { THEME } from "src/theme";
+import { View, VStack } from "native-base";
 
 export function ClassHoursList() {
-
+  const items = [
+    {
+      id: 1,
+      dayOfWeek: '1',
+      hours: [
+        { start: '7:00', end: '8:00' },
+        { start: '8:00', end: '9:00' },
+        { start: '9:00', end: '10:00' },
+        { start: '10:00', end: '11:00' }
+      ]
+    },
+    {
+      id: 2,
+      dayOfWeek: '2',
+      hours: [
+        { start: '7:00', end: '8:00' },
+        { start: '8:00', end: '9:00' },
+        { start: '9:00', end: '10:00' },
+        { start: '10:00', end: '11:00' }
+      ]
+    },
+    {
+      id: 3,
+      dayOfWeek: '3',
+      hours: [
+        { start: '7:00', end: '8:00' },
+        { start: '8:00', end: '9:00' },
+        { start: '9:00', end: '10:00' },
+        { start: '10:00', end: '11:00' }
+      ]
+    },
+    {
+      id: 4,
+      dayOfWeek: '4',
+      hours: [
+        { start: '7:00', end: '8:00' },
+        { start: '8:00', end: '9:00' },
+        { start: '9:00', end: '10:00' },
+        { start: '10:00', end: '11:00' }
+      ]
+    }
+  ]
   return (
     <View flex={1}>
       <PageHeader title="Jornadas" />
       <ScrollContainer>
-        <ClassDayHourItem />
+        <VStack space={4}>
+          {
+            items && items.length && (
+              items.map((item) => {
+                return (
+                  <ClassHourItem key={item.id} item={item} />
+                )
+              })
+            )
+          }
+        </VStack>
+
       </ScrollContainer>
     </View >
   )
