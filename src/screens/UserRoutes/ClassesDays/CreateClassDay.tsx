c7a83927018d03394d43cb5cacac275783aa1390
Mateus Lopes <devmateustools@gmail.com>
2024-08-23 02:29:06 -0300

    feat: crate class day form

--- src/screens/UserRoutes/ClassesDays/CreateClassDay.tsx
+++ src/screens/UserRoutes/ClassesDays/CreateClassDay.tsx
@@ -2,9 +2,13 @@ import { Input } from "@components/Input";
 import { PageHeader } from "@components/PageHeader";
 import { ScrollContainer } from "@components/ScrollContainer";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { AddIcon, HStack, Select, Text, View, VStack } from "native-base";
+import { size } from "lodash";
+import { AddIcon, HStack, Select, Text, useTheme, View, VStack } from "native-base";
+import { background, border, color } from "native-base/lib/typescript/theme/styled-system";
 import { Check } from "phosphor-react-native";
 import { Controller, useForm } from "react-hook-form";
+import { TextInputMask } from "react-native-masked-text";
+import { THEME } from "src/theme";
 import { z } from "zod";
 
 
@@ -12,7 +16,7 @@ const createClassDaySchema = z.object({
   date: z.string(),
   hourStart: z.string(),
   hourEnd: z.string(),
-  classId: z.string(),
+  classId: z.string()
 });
 
 type createclassDayProps = z.infer<typeof createClassDaySchema>
@@ -23,37 +27,93 @@ export function CreateClassDay() {
     resolver: zodResolver(createClassDaySchema)
   });
 
-  const handleCreateClassDay = () => {
-    console.log('ok')
+  const { sizes, colors } = useTheme();
+
+  const handleCreateClassDay = (data: createclassDayProps) => {
+    console.log('ok: ', data)
   }
+
   return (
     <View flex={1}>
-      <PageHeader title="Criar aula" rightIcon={Check} rightAction={handleCreateClassDay} />
+      <PageHeader title="Criar aula" rightIcon={Check} rightAction={handleSubmit(handleCreateClassDay)} />
       <ScrollContainer>
-        <VStack space={6} mt={12}>
+        <VStack space={6} mt={4}>
+          <Text fontSize="sm" fontWeight="medium" color="coolGray.700" mb={-4}> Data: </Text>
           <Controller
             name="date"
             control={control}
             render={({ field: { onChange, value } }) => (
-              <Input label="Data" variant="outline" onChangeText={onChange} value={value} errorMessage={errors.date?.message} />
+              <TextInputMask
+                style={{
+                  width: '100%',
+                  borderBottomColor: 'red',
+                  color: colors.coolGray[700],
+                  height: sizes[10],
+                  borderColor: colors.coolGray[300],
+                  borderWidth: 0.8,
+                  borderRadius: 4,
+                  paddingLeft: sizes[4]
+                }}
+                type={'datetime'}
+                options={{
+                  format: 'DD/MM/YYYY'
+                }} onChangeText={onChange} value={value}
+              />
+
             )}
           />
           <HStack space={4} w={'48%'}>
-            <Controller
-              name="hourStart"
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <Input label="Horário inicial:" variant="outline" onChangeText={onChange} value={value} errorMessage={errors.hourStart?.message} />
-              )}
-            />
+            <VStack space={4} w={'100%'}>
+              <Text fontSize="sm" fontWeight="medium" color="coolGray.700"> Horário de inicio: </Text>
+              <Controller
+                name="hourStart"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextInputMask
+                    style={{
+                      width: '100%',
+                      borderBottomColor: 'red',
+                      color: colors.coolGray[700],
+                      height: sizes[10],
+                      borderColor: colors.coolGray[300],
+                      borderWidth: 0.8,
+                      borderRadius: 4,
+                      paddingLeft: sizes[4]
+                    }}
+                    type={'datetime'}
+                    options={{
+                      format: 'HH:mm'
+                    }} onChangeText={onChange} value={value}
+                  />
+                )}
+              />
+            </VStack>
 
-            <Controller
-              name="hourEnd"
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <Input label="Horário final:" variant="outline" onChangeText={onChange} value={value} errorMessage={errors.hourEnd?.message} />
-              )}
-            />
+            <VStack space={4} w={'100%'}>
+              <Text fontSize="sm" fontWeight="medium" color="coolGray.700"> Horário de término: </Text>
+              <Controller
+                name="hourEnd"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextInputMask
+                    style={{
+                      width: '100%',
+                      borderBottomColor: 'red',
+                      color: colors.coolGray[700],
+                      height: sizes[10],
+                      borderColor: colors.coolGray[300],
+                      borderWidth: 0.8,
+                      borderRadius: 4,
+                      paddingLeft: sizes[4]
+                    }}
+                    type={'datetime'}
+                    options={{
+                      format: 'HH:mm'
+                    }} onChangeText={onChange} value={value}
+                  />
+                )}
+              />
+            </VStack>
 
           </HStack>
 
@@ -62,7 +122,7 @@ export function CreateClassDay() {
             name="classId"
             control={control}
             render={({ field: { onChange, value } }) => (
-              <Select accessibilityLabel="Choose Service" variant="outline" mt={-2}>
+              <Select accessibilityLabel="Selecione a turma" selectedValue={value} variant="outline" mt={-2} onValueChange={onChange}>
                 <Select.Item label="UX Research" value="ux" />
                 <Select.Item label="Web Development" value="web" />
                 <Select.Item label="Cross Platform Development" value="cross" />
