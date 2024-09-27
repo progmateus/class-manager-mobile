2a3d1293634b73c1f11f1a708c4e54918f504c82
Mateus Lopes <devmateustools@gmail.com>
2024-09-27 03:52:14 -0300

    feat: errors users and bookings

---
+++ src/helpers/errors.ts
@@ -0,0 +1,91 @@
+const errors = [
+  {
+    message: "ERR_INVALID_CREDENTIALS",
+    description: "E-mail ou senha inválidos"
+  },
+  {
+    message: "ERR_USER_INACTIVE",
+    description: "Usuário inativo"
+  },
+  {
+    message: "ERR_VALIDATION",
+    description: "Dados inválidos"
+  },
+  {
+    message: "ERR_USER_NOT_FOUND",
+    description: "Usuário não encontrado"
+  },
+  {
+    message: "ERR_TENANT_INACTIVE",
+    description: "A empresa está inativa"
+  },
+  {
+    message: "ERR_CLASS_DAY_NOT_FOUND",
+    description: "Aula não encontrada"
+  },
+  {
+    message: "ERR_CLASS_NOT_PENDING",
+    description: "A aula não está pendente"
+  },
+  {
+    message: "ERR_ADMIN_ROLE_NOT_FOUND",
+    description: "Apenas administradores podem acessar este recurso"
+  },
+  {
+    message: "ERR_STUDENT_ROLE_NOT_FOUND",
+    description: "Apenas alunos podem acessar este recurso"
+  },
+  {
+    message: "ERR_SUBSCRIPTION_NOT_FOUND",
+    description: "Inscrição não encontrada"
+  },
+  {
+    message: "ERR_SUBSCRIPTION_NOT_ACTIVE",
+    description: "Inscrição não inativa"
+  },
+  {
+    message: "ERR_NOT_CLASS_STUDENT",
+    description: "O aluno não faz parte da turma"
+  },
+  {
+    message: "ERR_TENANT_PLAN_NOT_FOUND",
+    description: "Plano não encontrado"
+  },
+  {
+    message: "ERR_WEEK_TIMES_EXCEEDED",
+    description: "Limite de aulas semanais excedido"
+  },
+  {
+    message: "ERR_BOOKING_ALREADY_EXISTS",
+    description: "Esta check-in já foi feito"
+  },
+  {
+    message: "ERR_USER_ROLE_NOT_FOUND",
+    description: "Acesso negado"
+  },
+  {
+    message: "ERR_BOOKING_NOT_FOUND",
+    description: "Check-in não encontrado"
+  },
+  {
+    message: "ERR_CLASS_DAY_ALREADY_CONCLUDED",
+    description: "Esta aula já foi concluida"
+  },
+  {
+    message: "ERR_BOOKING_ALREADY_EXISTS",
+    description: "Esta aula já foi agendada"
+  },
+  {
+    message: "ERR_BOOKING_ALREADY_EXISTS",
+    description: "Esta aula já foi agendada"
+  },
+  {
+    message: "ERR_BOOKING_ALREADY_EXISTS",
+    description: "Esta aula já foi agendada"
+  },
+  {
+    message: "ERR_BOOKING_ALREADY_EXISTS",
+    description: "Esta aula já foi agendada"
+  }
+]
+export { errors }
