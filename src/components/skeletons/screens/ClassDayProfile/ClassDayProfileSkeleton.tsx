bf62772b8336ad6f75ee172172acddac5c2b0940
Mateus Lopes <devmateustools@gmail.com>
2024-10-02 14:43:17 -0300

    feat: screen class day profile using skeleton

--- src/components/skeletons/screens/ClassDayProfile/ClassDayProfileSkeleton.tsx
+++ src/components/skeletons/screens/ClassDayProfile/ClassDayProfileSkeleton.tsx
@@ -1,33 +1,34 @@
 import { Viewcontainer } from "@components/ViewContainer";
 import { HStack, View, VStack } from "native-base";
 import { StudentItemSkeleton } from "../../Items/StudentItemSkeleton";
+import { Skeleton } from "@components/skeletons";
 
 export function ClassDayProfileSkeleton() {
   return (
     <Viewcontainer>
       <VStack space={2}>
         <HStack alignItems="center" space={3}>
-          <View width={6} height={6} bgColor="coolGray.200" />
-          <View width="56" height={3} bgColor="coolGray.200" />
+          <Skeleton width={6} height={6} />
+          <Skeleton width="56" height={3} />
         </HStack>
 
         <HStack alignItems="center" space={3}>
-          <View width={6} height={6} bgColor="coolGray.200" />
-          <View width="32" height={3} bgColor="coolGray.200" />
+          <Skeleton width={6} height={6} />
+          <Skeleton width="32" height={3} />
         </HStack>
 
         <HStack alignItems="center" space={3}>
-          <View width={6} height={6} bgColor="coolGray.200" />
-          <View width="24" height={3} bgColor="coolGray.200" />
+          <Skeleton width={6} height={6} />
+          <Skeleton width="24" height={3} />
         </HStack>
 
         <HStack alignItems="center" space={3}>
-          <View width={6} height={6} bgColor="coolGray.200" />
-          <View width="32" height={3} bgColor="coolGray.200" />
+          <Skeleton width={6} height={6} />
+          <Skeleton width="32" height={3} />
         </HStack>
       </VStack>
       <View mt={8} ml={2} flex={1}>
-        <View width="32" height={4} mb={4} bgColor="coolGray.200" />
+        <Skeleton width="32" height={4} mb={4} />
         <VStack space={5}>
           <StudentItemSkeleton />
           <StudentItemSkeleton />
