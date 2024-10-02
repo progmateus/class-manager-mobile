2cc830390f84cc92cc7c025e573808fe498065cd
Mateus Lopes <devmateustools@gmail.com>
2024-10-02 14:45:12 -0300

    feat: class day item using skeleton

--- src/components/skeletons/Items/ClassDayItemSkeleton.tsx
+++ src/components/skeletons/Items/ClassDayItemSkeleton.tsx
@@ -1,21 +1,22 @@
 import { HStack, View, VStack } from "native-base";
+import { Skeleton } from "..";
 
 export function ClassDayItemSkeleton() {
   return (
     <HStack px={6} py={5} space={6} alignItems="center" borderWidth={0.4} borderColor="coolGray.400" rounded="lg">
       <VStack space={2.5} alignItems="center" justifyContent="center">
-        <View size={6} bgColor="coolGray.200" />
-        <View w={8} h={3} bgColor="coolGray.200" />
+        <Skeleton size={6} />
+        <Skeleton w={8} h={3} />
       </VStack>
       <VStack space={2.5} flex={1} justifyContent="center">
-        <View w={24} h={3} bgColor="coolGray.200" />
-        <View w={16} h={2} bgColor="coolGray.200" />
-        <View w={9} h={2} bgColor="coolGray.200" />
+        <Skeleton w={24} h={3} />
+        <Skeleton w={16} h={2} />
+        <Skeleton w={9} h={2} />
       </VStack>
       <HStack alignItems="center" justifyContent="center">
-        <View w={9} h={9} bgColor="coolGray.200" borderColor="white" borderWidth={1} />
-        <View w={9} h={9} bgColor="coolGray.200" borderColor="white" borderWidth={1} ml={-5} />
-        <View w={9} h={9} bgColor="coolGray.200" borderColor="white" borderWidth={1} ml={-5} />
+        <Skeleton w={9} h={9} borderColor="white" borderWidth={1} />
+        <Skeleton w={9} h={9} borderColor="white" borderWidth={1} ml={-5} />
+        <Skeleton w={9} h={9} borderColor="white" borderWidth={1} ml={-5} />
       </HStack>
     </HStack>
   )
--- src/components/skeletons/Items/StudentItemSkeleton.tsx
+++ src/components/skeletons/Items/StudentItemSkeleton.tsx
@@ -1,14 +1,15 @@
 import { HStack, View, VStack } from "native-base";
+import { Skeleton } from "..";
 
 export function StudentItemSkeleton() {
   return (
     <HStack alignItems="center" rounded="md">
       <VStack space={2.5} alignItems="center" justifyContent="center">
-        <View size="10" bgColor="coolGray.200" rounded="full" />
+        <Skeleton size="10" rounded="full" />
       </VStack>
       <VStack flex={1} ml={3} justifyContent="center" space={2}>
-        <View w={24} h={3} bgColor="coolGray.200" />
-        <View w={16} h={2} bgColor="coolGray.200" />
+        <Skeleton w={24} h={3} />
+        <Skeleton w={16} h={2} />
       </VStack>
     </HStack>
   )
