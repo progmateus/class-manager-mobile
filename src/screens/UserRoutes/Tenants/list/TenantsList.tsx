12bb5dc81346cdf6a4d65c2ad605c8d732ad92d4
Mateus Lopes <devmateustools@gmail.com>
2024-10-03 21:49:47 -0300

    feat: tenants list margin top

--- src/screens/UserRoutes/Tenants/list/TenantsList.tsx
+++ src/screens/UserRoutes/Tenants/list/TenantsList.tsx
@@ -25,115 +25,6 @@ type serahcProps = {
 }
 
 export function TenantsList() {
-  const items = [
-    {
-      id: '1',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '2',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }, {
-      id: '3',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '4',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }, {
-      id: '5',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '6',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }, {
-      id: '7',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '8',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }, {
-      id: '9',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '10',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }, {
-      id: '11',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '12',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }, {
-      id: '13',
-      image: "https://imagens.ebc.com.br/-ZvfoGkRxj3IGhth9K5DyZix0AU=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/e38skrzx0aythza.jpg?itok=obCfIY5Q",
-      name: "Vôlei Paralelo",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      username: "voleiresenha",
-      categories: ["Vôlei", "Esporte", "Lazer"]
-    },
-    {
-      id: '14',
-      image: "https://imply.com/wp-content/uploads/female-professional-volleyball-players-action-3d-stadium-750x500.jpg",
-      name: "Vôlei Resenha",
-      username: "voleiresenha",
-      description: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-      categories: ["Vôlei", "Lazer", "Brigas"]
-    }
-  ]
-
   const [tenants, setTenants] = useState<ITenant[]>([])
   const [search, setSearch] = useState("")
 
@@ -160,11 +51,11 @@ export function TenantsList() {
   }
 
   return (
-    <VStack flex={1} mt={statusBarHeight + 18}>
+    <VStack flex={1} mt={statusBarHeight + 12}>
       <View px={4}>
         <Input
           value={search}
-          onChangeText={(e) => handleSearch(e)}
+          onChangeText={handleSearch}
           placeholder="Buscar"
           InputLeftElement={<Icon as={MagnifyingGlass} style={{ marginLeft: 8 }} color="coolGray.400" />}
         />
