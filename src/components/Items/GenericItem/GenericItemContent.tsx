091b2849b722faab3affad50b8ba0a52cc01ed01
Mateus Lopes <devmateustools@gmail.com>
2024-12-23 21:46:24 -0300

    feat: generic item with optional parameters

--- src/components/Items/GenericItem/GenericItemContent.tsx
+++ src/components/Items/GenericItem/GenericItemContent.tsx
@@ -1,13 +1,13 @@
 import { Heading, Text, VStack } from "native-base"
 type IProps = {
-  title: string;
-  caption: string;
+  title?: string;
+  caption?: string;
 }
 export function GenericItemContent({ title, caption }: IProps) {
   return (
     <VStack flex={1} space={1}>
-      <Text textTransform="capitalize" fontSize="md" color="coolGray.900"> {title}</Text>
-      <Text fontSize="xs" color="coolGray.500"> {caption}</Text>
+      {title && <Text textTransform="capitalize" fontSize="md" color="coolGray.900"> {title}</Text>}
+      {caption && <Text fontSize="xs" color="coolGray.500"> {caption}</Text>}
     </VStack>
   )
 }
